9e67d90a76ca7902acf67286a07e1aad0e115be8
Anjali <80470285+Anjali655@users.noreply.github.com>
2025-10-04 14:33:02 +0530

    Create spiralOrder.cpp

---
+++ spiralOrder.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+  int n = matrix.size();
+    if (n == 0)
+        return {};
+    int m = matrix[0].size();
+
+    // Initialize boundary pointers.
+    int top = 0;
+    int bottom = n - 1;
+    int left = 0;
+    int right = m - 1;
+
+    vector<int> ans;
+
+    while (top <= bottom && left <= right)
+    {
+        // Traverse right.
+        for (int i = left; i <= right; i++)
+        {
+            ans.push_back(matrix[top][i]);
+        }
+        top++;
+
+        // Traverse down.
+        for (int i = top; i <= bottom; i++)
+        {
+            ans.push_back(matrix[i][right]);
+        }
+        right--;
+
+        // Traverse left.
+        if (top <= bottom)
+        {
+            for (int i = right; i >= left; i--)
+            {
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        // Traverse up.
+        if (left <= right)
+        {
+            for (int i = bottom; i >= top; i--)
+            {
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    return ans;
+};
+};
